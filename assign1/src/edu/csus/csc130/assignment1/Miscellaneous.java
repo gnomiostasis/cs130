fc02652b8b99df1e4a32eb9482c711b63f59a5cf
gnomiostasis <Zack@zacharys-mbp.attlocal.net>
2016-09-26 17:46:18 -0700

    2 steps of 3 done
    Binary search, select elements complete

--- assign1/src/edu/csus/csc130/assignment1/Miscellaneous.java
+++ assign1/src/edu/csus/csc130/assignment1/Miscellaneous.java
@@ -1,5 +1,7 @@
 package edu.csus.csc130.assignment1;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -10,17 +12,77 @@ public class Miscellaneous {
 	
 	public static long powI(long x, int n) {
 		// provide your implementation here
-		throw new UnsupportedOperationException();
+		try 
+		{
+			if(n == 0)
+			{
+				return 1;
+			}
+			 
+			if (n == 1)
+			{
+				return x;
+			}
+			 
+		}
+		catch (Exception e)
+		{ 
+			throw e;
+		}
+		return 0;
+			 
+//			 if (n % 2 == 0)
+//			 return powR(x * x, n / 2);
+//			 else
+//			 return powR(x * x, n / 2) * x;
 	}
 	
 	public static int binarySearch(int[] a, int key) {
 		// provide your implementation here
-		throw new UnsupportedOperationException();
+		int return_index=-1;
+		try{
+			int low = 0;
+			int high = a.length - 1;
+		          
+			while(high >= low) {
+		        int middle = (low + high) / 2;
+		        if(a[middle] == key) {
+		            return_index = middle;
+		        }
+		        if(a[middle] < key) {
+		             low = middle + 1;
+		        }
+		        if(a[middle] >= key) {
+		        	 high = middle - 1;
+		        }
+		    }
+		}
+		catch(Exception e)
+		{
+			throw e;
+		}
+		return return_index;
 	}
 	
 	public static<E> List<E> getSelected(List<E> items, List<Integer> positions) {
 		// provide your implementation here
-		throw new UnsupportedOperationException();
+		List <E> return_list = new ArrayList<E>();
+		try{
+			Iterator itr = positions.iterator();
+			while (itr.hasNext())
+			{
+				Integer pos = (Integer)itr.next();
+				if (pos.intValue() >=0 && pos.intValue()<items.size())
+				{
+					return_list.add(items.get(pos));
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			throw e;
+		}
+		return return_list;
 	}
 
 }
