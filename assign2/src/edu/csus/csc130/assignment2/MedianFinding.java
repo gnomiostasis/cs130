eedb2a806742cbf5faf0e6fec057d7e7e1d585ff
gnomiostasis <gnomiostasis@bobs-imac.attlocal.net>
2016-10-16 21:24:32 -0700

    Finished assign2
    Added sorting, merging, and other shit.

--- assign2/src/edu/csus/csc130/assignment2/MedianFinding.java
+++ assign2/src/edu/csus/csc130/assignment2/MedianFinding.java
@@ -15,18 +15,119 @@ public class MedianFinding {
 	
 	// worst case running time: O(lgn) 
 	public void insert(int n) {
-		throw new UnsupportedOperationException();
+		
+		 if (minPQ.size() == 0)
+		 {
+			 minPQ.insert(n);
+		 }
+		 else if (maxPQ.size() == 0)
+		 {
+			 maxPQ.insert(n); 
+		 }
+		 else
+		 {
+			 if (n>minPQ.min())
+			 {
+				 minPQ.insert(n); //if N is generally larger
+			 }
+			 else
+			 {
+				 maxPQ.insert(n); //if N is generally smaller
+			 }
+		 }
+			 
+		 balanceQueues();
 	}
 	
 	// worse case running time: O(1)
 	public double getMedian() {
-		throw new UnsupportedOperationException();
+		if (maxPQ.size() ==minPQ.size())
+		{
+			return (maxPQ.max() + minPQ.min())/2.0;
+		}
+		else if(maxPQ.size()>minPQ.size())
+		{
+			return (double) maxPQ.max();
+		}
+		else
+		{
+			return (double) minPQ.min();
+		}
 	}
 	
 	// worst case running time: O(lgn)
 	// If there are even number of elements, delete the larger one of the middle two
 	public void deleteMedian() {
-		throw new UnsupportedOperationException();
+		if (maxPQ.size() == minPQ.size())
+		{
+			minPQ.delMin();
+		}
+		else if(maxPQ.size()>minPQ.size())
+		{
+			maxPQ.delMax();
+		}
+		else
+		{
+			minPQ.delMin();
+		}
+	}
+	
+	private void balanceQueues()
+	{
+		
+		if (!isBalancedQueues())
+		{
+			redistribute();
+		}
+		if (maxPQ.size() != 0 && minPQ.size()!=0)
+		{
+			if (maxPQ.max()>minPQ.min())
+			{
+				swapMinMax();
+			}	
+		}
+		
+		
+	}
+	private boolean isBalancedQueues()
+	{
+		//tests to see if maxpq and minpq are equal size +/- 1
+		if( maxPQ.size() == minPQ.size()
+			|| maxPQ.size()-1 == minPQ.size()
+			|| maxPQ.size() == minPQ.size()-1)
+		{
+			return true;
+		}
+		return false;
+	}
+	private void redistribute()
+	{
+		if (maxPQ.size()>minPQ.size())
+		{
+			while (!isBalancedQueues())
+			{
+				minPQ.insert(maxPQ.max());
+				maxPQ.delMax();
+			}
+		}
+		else
+		{
+			while (!isBalancedQueues())
+			{
+				maxPQ.insert(minPQ.min());
+				minPQ.delMin();
+			}
+		}
+	}
+	
+	private void swapMinMax()
+	{
+		int frommax = maxPQ.max();
+		int frommin = minPQ.min();
+		maxPQ.delMax();
+		minPQ.delMin();
+		maxPQ.insert(frommin);
+		minPQ.insert(frommax);
 	}
 	
 }
--- assign2/src/edu/csus/csc130/assignment2/Sorting.java
+++ assign2/src/edu/csus/csc130/assignment2/Sorting.java
@@ -1,8 +1,8 @@
 package edu.csus.csc130.assignment2;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-
 
 /**
  * Modified by firstname lastname
@@ -18,37 +18,108 @@ public class Sorting {
 	public static <T extends Comparable<T>> List<T> merge(List<T> list1, List<T> list2) {
 		List<T> list = new LinkedList<T>();
 		
-		// provide implementation here
-		int list1_index = 0;
-		int list2_index = 0;
+		Iterator iter1 = list1.iterator();
+		Iterator iter2 = list2.iterator();
+		T item1 = (T)iter1.next();
+		T item2 = (T)iter2.next();
 		int combined_size = list1.size() + list2.size();
-		while (list.size()<=combined_size)
+		while (list.size()!=combined_size)
 		{
-			T item1 = list1.get(list1_index);
-			T item2 = list2.get(list2_index);
-			//if item1<item2
-			if (isLessThan(item2,item1))
+			if( item1 != null && item2 != null)
 			{
-				list.add(item1);
-				list1_index++;
+				if (isLessThan(item1,item2))
+				{
+					list.add(item1);
+					try
+					{
+						item1 = (T)iter1.next();
+					}
+					catch(Exception e)
+					{
+						item1 = null;
+					}
+					
+				}
+				else
+				{
+					list.add(item2);
+					try
+					{
+						item2 = (T)iter2.next();
+					}
+					catch(Exception e)
+					{
+						item2 = null;
+					}
+				}
 			}
 			else
 			{
-				list.add(item2);
-				list2_index++;
+				if (item1 == null && item2 != null)
+				{
+					list.add(item2);
+					try
+					{
+						item2 = (T)iter2.next();
+					}
+					catch(Exception e)
+					{
+						item2 = null;
+					}
+				}
+				
+				if(item2 == null && item1 != null)
+				{
+					list.add(item1);
+					try
+					{
+						item1 = (T)iter1.next();
+					}
+					catch(Exception e)
+					{
+						item1 = null;
+					}
+				}
 			}
+			
 		}
 		
 		return list;
 	}
 	
+	
+
 	/**
 	 * @param a String array where each element has one of the three values: "false", "maybe", or "true"
 	 * Give an O(N) algorithm to rearrange the elements so that all �false� elements precede �maybe� elements, 
 	 * which in turn precede �true� elements. You may use only constant extra space.  
 	 */
 	public static void rearrange(String[] a) {
-		throw new UnsupportedOperationException();
+		int falsepos = 0;
+		int truepos = a.length-1;
+		for (int i =0 ; i < a.length; i++)
+		{
+			switch (a[i].toLowerCase())
+			{
+			case "false":
+				if(i>falsepos)
+				{
+					swap(a,i,falsepos);
+					falsepos++;
+				}
+				break;
+			case "maybe":
+				break;
+			case "true":
+				if(i<truepos)
+				{
+					swap(a,i,truepos);
+					truepos--;
+				}
+				
+				break;
+			}
+		}
 	}
 	
 	public static<T extends Comparable<T>> boolean isLessThan(T v, T w) {
